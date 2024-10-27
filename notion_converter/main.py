189bb27f7c9c81ae2c030f5c9bd61076626db294
EG <96171393+egubaidullin@users.noreply.github.com>
2024-10-27 12:27:03 +0700

    Update main.py

--- notion_converter/main.py
+++ notion_converter/main.py
@@ -6,12 +6,16 @@ from template_engine import TemplateEngine
 from html_renderer import HTMLRenderer
 from file_manager import FileManager
 
-def generate_navigation(pages_data: List[Tuple[str, str]]) -> str:
-    """Генерирует навигацию по страницам"""
+def generate_navigation(pages_data: List[Tuple[str, str]], template_engine) -> str:
+    """Генерирует HTML навигацию по страницам"""
     nav_items = []
     for title, filename in pages_data:
-        nav_items.append(f'<a href="{filename}.html">{title}</a>')
-    return ' '.join(nav_items)
+        # Используем путь к шаблону для каждого элемента навигации
+        nav_items.append(template_engine.render_page("blocks/navigation_item.html", title=title, url=f"{filename}.html"))
+    return template_engine.render_page("blocks/navigation.html", navigation_items=''.join(nav_items))
+
+
+
 
 def main():
     # Настройка логирования
@@ -48,25 +52,8 @@ def main():
             filename = 'index' if idx == 0 else f'page_{idx + 1}'
             pages_data.append((title, filename))
             
-            # Сохранение страницы
-            file_manager.save_html(
-                filename,
-                template_engine.render_page('template.html',
-                    title=title,
-                    content=html_content,
-                    toc=toc,
-                    navigation=''
-                )
-            )
-
-        # Генерация навигации и обновление страниц
-        navigation = generate_navigation(pages_data)
-        for title, filename in pages_data:
-            page_id = page_ids[pages_data.index((title, filename))]
-            notion_content = notion_client.get_page_content(page_id)
-            html_content = html_renderer.convert_to_html(notion_content)
-            toc = html_renderer.generate_toc(notion_content)
-            
+            # Сохранение страницы с навигацией
+            navigation = generate_navigation(pages_data, template_engine)
             file_manager.save_html(
                 filename,
                 template_engine.render_page('template.html',
